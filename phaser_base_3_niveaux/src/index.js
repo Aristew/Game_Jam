7972fe20118ba1201e9fe925cc915203823212d6
Ethan_Cavallo <ethan.cavallo@epfedu.fr>
2025-03-10 14:25:29 +0100

    animations tjrs

--- phaser_base_3_niveaux/src/index.js
+++ phaser_base_3_niveaux/src/index.js
@@ -53,16 +53,16 @@ function preload() {
    this.load.image("img_ciel", "src/assets/sky.png"); 
    this.load.image("img_plateforme", "src/assets/platform.png");  
    this.load.spritesheet("img_perso", "src/assets/Idle.png", {
-    frameWidth: 32,
-    frameHeight: 48
+    frameWidth: 128,
+    frameHeight: 128
   }); 
   this.load.spritesheet("gauche", "src/assets/Run2.png", {
-    frameWidth: 32,
-    frameHeight: 48
+    frameWidth: 128,
+    frameHeight: 128
   }); 
   this.load.spritesheet("droite", "src/assets/Run.png", {
-    frameWidth: 32,
-    frameHeight: 48
+    frameWidth: 128,
+    frameHeight: 128
   }); 
 }
 
@@ -113,13 +113,13 @@ function create() {
   }); 
   this.anims.create({
     key: "anim_tourne_droite", // key est le nom de l'animation : doit etre unique poru la scene.
-    frames: this.anims.generateFrameNumbers("droite", { start: 0, end: 7 }), // on prend toutes les frames de img perso numerotées de 0 à 3
+    frames: this.anims.generateFrameNumbers("droite", { start: 7, end: 0 }), // on prend toutes les frames de img perso numerotées de 0 à 3
     frameRate: 10, // vitesse de défilement des frames
     repeat: -1 // nombre de répétitions de l'animation. -1 = infini
   }); 
   this.anims.create({
     key: "anim_face", // key est le nom de l'animation : doit etre unique poru la scene.
-    frames: this.anims.generateFrameNumbers("img_perso", { start: 4, end: 4 }), // on prend toutes les frames de img perso numerotées de 0 à 3
+    frames: this.anims.generateFrameNumbers("img_perso", { start: 0, end: 7 }), // on prend toutes les frames de img perso numerotées de 0 à 3
     frameRate: 10, // vitesse de défilement des frames
     repeat: -1 // nombre de répétitions de l'animation. -1 = infini
   }); 
