6b83322ef22c3b85df161b15dc4eb1bf888ef516
Ethan_Cavallo <ethan.cavallo@epfedu.fr>
2025-03-11 15:45:15 +0100

    ajout de la destruction de la box

--- phaser_base_3_niveaux/src/index.js
+++ phaser_base_3_niveaux/src/index.js
@@ -17,6 +17,7 @@ var texteCompteur;
 var scene;
 var musique_de_fond;
 var Squelette_1;
+var box;
 
 function tirerProjectile(type, player) {
   var coefDir = (player.direction == 'left') ? -1 : 1;
@@ -32,13 +33,19 @@ function tirerProjectile(type, player) {
   bullet.setDisplaySize(20, 20);
   bullet.setCollideWorldBounds(false);
   bullet.body.onWorldBounds = true;
-  bullet.body.allowGravity = true;  // Activation de la gravité
-  bullet.setVelocity(300 * coefDir, 0); // Moins de vitesse horizontale, tir plus haut
+  bullet.body.allowGravity = true;
+  bullet.setVelocity(450 * coefDir, 0);
 
-  // Ajouter la collision entre le projectile et le squelette
+  // Collision avec un squelette
   scene.physics.add.overlap(bullet, Squelette_1, () => {
-    Squelette_1.disableBody(true, true); // Désactiver le squelette
-    bullet.destroy(); // Détruire le projectile
+    Squelette_1.disableBody(true, true);
+    bullet.destroy();
+  });
+
+  // Collision avec une box
+  scene.physics.add.overlap(bullet, box, () => {
+    box.disableBody(true, true);
+    bullet.destroy();
   });
 }
 
@@ -261,7 +268,8 @@ function preload() {
    // chargement tuiles de jeu
    this.load.image("Phaser_tuilesdejeu", "src/assets/Tileset.png");
    this.load.image("fond", "src/assets/fond.png");
-   this.load.image("box", "src/assets/box.png");
+
+   box=this.load.image("box", "src/assets/box.png");
 
    this.load.spritesheet("esprit", "src/assets/spirit.png", { 
     frameWidth: 128, 
@@ -428,7 +436,6 @@ this.physics.add.collider(Squelette_1, plateforme);
 // Création de la bulle de texte (initialement cachée)
 
 
-
   this.anims.create({
     key: "anim_tourne_gauche", // key est le nom de l'animation : doit etre unique poru la scene.
     frames: this.anims.generateFrameNumbers("gauche", { start: 7, end: 0 }), // on prend toutes les frames de img perso numerotées de 0 à 3
@@ -450,7 +457,7 @@ this.physics.add.collider(Squelette_1, plateforme);
   groupeBullets = scene.physics.add.group();
   groupe_mineraux = this.physics.add.group(); 
 
-  let box = this.physics.add.sprite(2450, 100, "box"); // Création du sprite
+  box = this.physics.add.sprite(2450, 100, "box"); // Création du sprite
   box.setCollideWorldBounds(true); // Empêche la box de sortir du monde
   box.setImmovable(true); // La box ne bougera pas si elle est touchée
   this.physics.add.collider(player, box); // Permet au joueur de rentrer en collision avec la box
@@ -460,6 +467,8 @@ this.physics.add.collider(Squelette_1, plateforme);
   // Liste des positions prédéfinies pour les minéraux
 let positionsMineraux = [
   { x: 500, y: 300, type: "rouge" },
+  { x: 575, y: 310, type: "violet" },
+  { x: 555, y: 300, type: "blanc" },
   { x: 650, y: 250, type: "blanc" },
   { x: 900, y: 275, type: "rose" },
   { x: 1300, y: 320, type: "violet" },
@@ -479,10 +488,10 @@ let positionsMineraux = [
   { x: 4600, y: 500, type: "orange" },
   { x: 5000, y: 350, type: "rouge" },
   { x: 5400, y: 400, type: "jaune_clair" },
-  { x: 5800, y: 100, type: "rose" },
-  { x: 6400, y: 100, type: "violet" },
-  { x: 6400, y: 100, type: "blanc" },
-  { x: 6400, y: 100, type: "orange" }
+  { x: 5800, y: 400, type: "rose" },
+  { x: 6400, y: 400, type: "violet" },
+  { x: 6400, y: 400, type: "blanc" },
+  { x: 6400, y: 400, type: "orange" }
 ];
 
 // Générer les minéraux à des positions fixes
